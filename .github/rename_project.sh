f55e265fb02158da0a052131b4161f1ceac4b92e
tonyqiu2020 <tonyqiu2020@users.noreply.github.com>
2021-10-29 20:24:27 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="tonyqiu2020"
+original_name="python_project_template"
+original_urlname="python-project-template"
+original_description="Awesome python_project_template created by tonyqiu2020"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv python_project_template $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
